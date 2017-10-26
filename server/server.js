48ba707a560ce47d1b001e27d8b290e47713cc9d
Juan R. Gavilanes <jrgavilanes@gmail.com>
2017-10-26 09:53:08 +0200

    3

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 let app = new express();
 
 app.get('/', (req, res) => {
-  res.send('hola nano!!');
+  res.send('hola nano!!!');
 });
 
 app.listen(PORT, () => {
