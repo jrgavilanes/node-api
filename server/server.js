9fe2b07699d17988353f0662822dec65baca3656
Juan R. Gavilanes <jrgavilanes@gmail.com>
2017-10-26 13:29:41 +0200

    bd por parametro DB_URL o local

--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ const PORT = process.env.PORT || 3000;
 
 let app = new express();
 
-
+console.log("pues: ", process.env.DB);
 
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://root:root@ds231315.mlab.com:31315/prueba', { useMongoClient: true });
+
+const URL_DB = process.env.URL_DB || "mongodb://127.0.0.1:27017/prueba";
+
+// mongoose.connect('mongodb://root:root@ds231315.mlab.com:31315/prueba', { useMongoClient: true });
+mongoose.connect(URL_DB, { useMongoClient: true });
 
 let Todo = mongoose.model('Todo', {
   titulo: {
