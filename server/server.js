86c66486fc949b417e8a8e2f8c4e6da9356ec258
Juan R. Gavilanes <jrgavilanes@gmail.com>
2017-10-26 10:05:45 +0200

    graba en mongo mlab

--- server/server.js
+++ server/server.js
@@ -4,8 +4,48 @@ const PORT = process.env.PORT || 3000;
 
 let app = new express();
 
+
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://root:root@ds231315.mlab.com:31315/prueba', { useMongoClient: true });
+
+let Todo = mongoose.model('Todo', {
+  titulo: {
+    type: String,
+    required: true,
+    unique: false,
+    minlength: 5,
+    trim: true
+  },
+  completado: {
+    type: Boolean,
+    default: false
+  },
+  modificado: {
+    type: Number,
+    default: null
+  }
+});
+
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('hola nano!!!');
+
+  let newTodo = new Todo({
+    titulo: 'Desde Heroku'
+  });
+
+  newTodo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.send("error: " + e);
+  });
+
 });
 
 app.listen(PORT, () => {
