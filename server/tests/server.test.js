3c0e5158f78f8fde561d77ad4c9f00cca32b4d80
Juanra <jrgavilanes@gmail.com>
2017-10-27 18:43:27 +0000

    tests suite por /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -52,3 +52,66 @@ describe('POST /todos', () => {
   });
 
 });
+
+describe("GET /todos", () => {
+  
+  it("should get all todos", (done) => {
+    var fakeData = [{ text: 'text 1' }, { text: 'text 2' }];
+    Todo.insertMany(fakeData).then(() => {
+      request(app).get('/todos')
+        .expect(200)
+        .end((err, res)=>{
+          if (err) {
+            return done(err);
+          }
+          //expect(JSON.parse(res.text).todos.length).toBe(2);
+          expect(res.body.todos.length).toBe(2);
+          done();
+        });
+    }).catch((e) => done(e));
+  });
+  
+});
+
+describe("GET /todos/id", () => {
+  
+  it("should get todos/id", (done) => {
+    var fakeData = [{ text: 'text 1' }, { text: 'text 2' }];
+    Todo.insertMany(fakeData).then((data) => {
+      var id = data[0]._id;
+      var text = data[0].text;
+      request(app).get(`/todos/${id}`)
+        .expect(200)
+        .end((err, res)=>{
+          if (err) {
+            return done(err);
+          }
+          expect(res.body.todo._id).toEqual(id);
+          expect(res.body.todo.text).toBe(text);
+          done();
+        }, (e) => done(e));
+    }).catch((e) => done(e));
+  });
+  
+  it("should Not get todos/id with an Invalid ID (404)", (done) => {
+    var fakeData = [{ text: 'text 1' }, { text: 'text 2' }];
+    Todo.insertMany(fakeData).then((data) => {
+      var id = data[0]._id;
+      var wrong_id = id + 'aaaaa';
+      request(app).get(`/todos/${wrong_id}`).expect(404);
+      done();
+    }).catch((e) => done(e));
+  });
+  
+  it("should Not get todos/id with an unknown ID (404)", (done) => {
+    var fakeData = [{ text: 'text 1' }, { text: 'text 2' }];
+    Todo.insertMany(fakeData).then((data) => {
+      var id = data[0]._id;
+      var wrong_id = id + '';
+      wrong_id = "1" + wrong_id.substr(1,30);
+      request(app).get(`/todos/${wrong_id}`).expect(404).end(done);
+      
+    }).catch((e) => done(e));
+  });
+  
+});
